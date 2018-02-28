7401cfb627c7b9798798cf1707826aa0e74cb7d8
Luke Garrigan <lukegarrigan8@gmail.com>
2018-02-28 21:07:47 +0000

    Expanding and shrinking the tasks work properly now

--- demo/src/main/webapp/js/myCtrl.js
+++ demo/src/main/webapp/js/myCtrl.js
@@ -280,7 +280,7 @@ app.controller("myCtrl", function ($scope, $http, $mdDialog) {
             .initialValue(task.message)
             .targetEvent(ev)
             .clickOutsideToClose(true)
-            .ok('Submit '  + task.message.length)
+            .ok('Submit ')
             .cancel('Cancel');
         $mdDialog.show(confirm).then(function(result) {
 
