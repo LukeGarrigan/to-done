ba0f886d3422b55b53bd02b53dcab9bdf5d7431e
Luke Garrigan <lukegarrigan8@gmail.com>
2018-03-31 17:27:25 +0100

    Have created a quick registration formr

--- demo/src/main/webapp/js/myApp.js
+++ demo/src/main/webapp/js/myApp.js
@@ -1,5 +1,12 @@
 var app = angular.module('myApp', ['ngMaterial', 'ui.sortable', 'ui.bootstrap.contextMenu']);
 
+
+app.directive("loginOrRegister", function(){
+    return {
+        templateUrl: 'login-or-register.html'
+    }
+});
+
 app.directive("addNewTask", function(){
     return {
         templateUrl: 'add-new-task.html'
