5505d7ca54b242b328573d146dd2776a9d2eb350
Luke Garrigan <lukegarrigan8@gmail.com>
2018-05-28 10:57:22 +0100

    added some more error handling on the registration

--- demo/src/main/webapp/js/registerController.js
+++ demo/src/main/webapp/js/registerController.js
@@ -2,16 +2,27 @@ app.controller("registerController", function ($scope, $http) {
 
 
     $scope.user = {};
-
     $scope.password = "";
     $scope.email = "";
-
     $scope.isAuthorised = false;
-
-    $scope.errorMessage = "";
+    $scope.loginErrorMessage = "";
+    $scope.registerErrorMessage = "";
 
     $scope.registerUser = function () {
+        if (!passwordsMatch()) {
+            $scope.registerErrorMessage = "Passwords do not match";
+        } else {
+            registerNewUser();
+        }
+    };
+
+    var passwordsMatch = function () {
+        return $scope.password === $scope.confirmPassword;
+    };
+
+    var registerNewUser = function () {
         $scope.user.password = $scope.password;
+        $scope.user.confirmPassword = $scope.confirmPassword;
         $scope.user.email = $scope.email;
 
         var userAsJson = JSON.stringify($scope.user);
@@ -21,14 +32,14 @@ app.controller("registerController", function ($scope, $http) {
                 'Content-Type': 'application/json'
             }
         }).then(function mySuccess(response) {
-            if (userAlreadyExists(response.data)) {
-
-            } else {
-                $scope.$parent.userId = response.data.id;
-                $scope.isAuthorised = true;
-            }
+            $scope.$parent.userId = response.data.id;
+            $scope.isAuthorised = true;
+        }, function myFailure(response) {
+            $scope.registerErrorMessage = response.data.message;
         });
+
     };
+
 
     $scope.loginUser = function () {
         $scope.user.password = $scope.password;
@@ -41,19 +52,14 @@ app.controller("registerController", function ($scope, $http) {
                 'Content-Type': 'application/json'
             }
         }).then(function mySuccess(response) {
-            if(response.data !== '' && response !== null) {
+            if (response.data !== '' && response !== null) {
                 $scope.$parent.userId = response.data.id;
                 $scope.isAuthorised = true;
             }
         }, function errorCallback(response) {
-            $scope.errorMessage = response.data.message;
+            $scope.loginErrorMessage = response.data.message;
         });
     };
-
-
-    function userAlreadyExists(data) {
-        return data === null || data === "";
-    }
 
 
 });
